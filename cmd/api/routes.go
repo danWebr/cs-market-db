61c5b9bd68352463809de80de67d2075e9f16e90
danWebr <danilo@weber-familie.de>
2023-08-17 20:15:31 +0200

    Add routing and item handler

---
+++ cmd/api/routes.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+func (app *application) routes() *httprouter.Router {
+	router := httprouter.New()
+
+	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/items", app.createItemHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/items/:id", app.showItemHandler)
+
+	return router
+}
